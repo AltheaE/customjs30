823fe7e97ea2ae251757e88460cb28929e995dc5
Althea <althea@metasolutions.se>
2018-10-25 12:24:50 +0200

    Add link on index

--- 08/main.js
+++ 08/main.js
@@ -16,12 +16,18 @@ let direction = true;
 function draw (e){
     if(!isDrawing)return;
     console.log(e);
+    ctx.strokeStyle = `hsl(${hue}, 60%, 50%)`;
     ctx.beginPath();
     ctx.moveTo(lastX, lastY);
     ctx.lineTo(e.offsetX,e.offsetY);
     ctx.stroke();
     lastX = e.offsetX;
     lastY = e.offsetY;
+
+    hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
 
     if (ctx.lineWidth >= 25 || ctx.lineWidth <= 1) {
         direction = !direction;
